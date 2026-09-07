0d9226ce6472ef9966029f436f80948636ca1ab2
a7med7ussein17 <ahmed.hussain1770@gmail.com>
2026-09-07 21:19:35 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,13 +11,10 @@ export default function Home() {
       name: "Creed Aventus (كريد أفينتوس)",
       category: "تركيب",
       badge: "توصيل مجاني لـ 30ml", 
-      {
-  
-  image: "https://iili.io/nHErxAF.png", // <--- استبدل هذا السطر فقط
-  
+      image: "https://iili.io/nHErxAF.png", //
       sizes: [
         { label: "10 مل", price: 5000, originalPrice: null, freeDelivery: false },
-        { label: "30 مل", price: 10000, originalPrice: 12000, freeDelivery: true }
+        { label: "30 مل", price: 12000, originalPrice: 15000, freeDelivery: true }
       ]
     },
     {
