5b2e592e6e562e13d78d412479220efd85482583
a7med7ussein17 <ahmed.hussain1770@gmail.com>
2026-09-11 16:59:44 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
       image: "https://iili.io/n3JOfqb.jpg",
 
       sizes: [
-        { label: "10 مل", price: 5000, originalPrice: null, freeDelivery: false },
+        { label: "10 مل", price: 10000, originalPrice: null, freeDelivery: false },
         { label: "30 مل", price: 23000, originalPrice: 25000, freeDelivery: true }
       ]
     }
